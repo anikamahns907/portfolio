441cf50bd2f7dc87cb1109a7d3619f78e167f6cc
anikamahns907 <64509221+anikamahns907@users.noreply.github.com>
2024-01-01 19:17:03 -0400

    particles react

--- portfolio/src/Home.js
+++ portfolio/src/Home.js
@@ -1,17 +1,111 @@
-import React from "react";
+import React, { useCallback, useEffect } from "react";
 import myFaceImage from './faceme.jpg';
 import './index.css';
+import { Container, Engine } from "tsparticles-engine";
+import Particles from "react-particles";
+//import { loadFull } from "tsparticles"; // if you are going to use `loadFull`, install the "tsparticles" package too.
+import { loadSlim } from "tsparticles-slim"; // if you are going to use `loadSlim`, install the "tsparticles-slim" package too.
 
+const Home = () => {
+  const particlesInit = useCallback(async (engine) => {
+    console.log(engine);
+    await loadSlim(engine); // Call loadSlim function here
+  }, []);
 
-class Home extends React.Component {
-  render() {
+  const particlesLoaded = useCallback(async (container) => {
+    await console.log(container);
+  }, []);
    
     return (
 
         <div className="mainDiv">
+
+      <Particles
+      className="particles"
+      id="tsparticles"
+      init={particlesInit}
+      loaded={particlesLoaded}
+      options={{
+        background: {
+          color: {
+            value: "white",
+          },
+        },
+        fpsLimit: 120,
+        interactivity: {
+          events: {
+            onClick: {
+              enable: true,
+              mode: "push",
+            },
+            onHover: {
+              enable: true,
+              mode: "repulse",
+            },
+            resize: true,
+          },
+          modes: {
+            push: {
+              quantity: 4,
+            },
+            repulse: {
+              distance: 190,
+              duration: 0.4,
+            },
+          },
+        },
+        particles: {
+          color: {
+            value: "#3D9EF6np",
+          },
+          links: {
+            color: "#3D9EF6",
+            distance: 200,
+            enable: true,
+            opacity: 0.5,
+            width: 1,
+          },
+          move: {
+            direction: "none",
+            enable: true,
+            outModes: {
+              default: "bounce",
+            },
+            random: false,
+            speed: 3,
+            straight: false,
+          },
+          number: {
+            density: {
+              enable: true,
+              area: 1000,
+            },
+            value: 100,
+          },
+          opacity: {
+            value: 0.5,
+          },
+          shape: {
+            type: "circle",
+          },
+          size: {
+            value: { min: 1, max: 5 },
+          },
+        },
+        detectRetina: true,
+      }}
+      style={{
+        position: "fixed",
+        zIndex: -1,
+        top: 0,
+        left: 0,
+        width: "100%",
+        height: "100%",
+      }}
+    />
                   
 
-        <div className="content">
+                  <div className="content" style={{ position: "relative", zIndex: 1 }}>
         <p className="name">Anika Mahns</p>
 
           <img src={myFaceImage} alt="My Face" className="my-face-image" />
@@ -53,6 +147,6 @@ class Home extends React.Component {
       </div>
 
     );
-  }
-}
-export default Home;
+  };
+
+  export default Home;
